67c0e78f6620d89959c2dc617b0961691854bbe7
Alec Matias <alec.matias@gmail.com>
2013-03-18 08:12:03 -0700

    Add permissions screen for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Ticketee::Application.routes.draw do
   namespace :admin do
     root :to => "base#index"
-    resources :users
+    resources :users do
+      resources :permissions
+    end
   end
 
   devise_for :users, :controllers => { :registrations => "registrations" }
@@ -9,6 +11,10 @@ Ticketee::Application.routes.draw do
   get "/awaiting_confirmation",
     :to => "users#confirmation",
     :as => "confirm_user"
+
+  put '/admin/users/:user_id/permissions',
+    :to => 'admin/permissions#update',
+    :as => :update_user_permissions
 
   root :to => "projects#index"
   resources :projects do
